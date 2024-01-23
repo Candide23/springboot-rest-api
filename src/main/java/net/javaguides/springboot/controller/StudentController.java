dfdf10ff9062ef062d90e1738d663f6c248e7dee
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-01-23 09:26:07 -0600

    worked on putMapping and requestBody

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -71,5 +71,14 @@ public class StudentController {
 
     }
 
+    @PutMapping("students/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+
+    }
+
 
 }
