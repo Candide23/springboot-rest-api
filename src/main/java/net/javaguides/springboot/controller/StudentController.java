c1ef0ec4d2a286f8a3a0edcfecde22dca1a52c38
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-01-23 09:12:28 -0600

    worked on postMapping and requestBody

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -2,10 +2,8 @@ package net.javaguides.springboot.controller;
 
 
 import net.javaguides.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,5 +54,22 @@ public class StudentController {
         return new Student(id, firstName, lastName);
 
     }
+
+    // spring boot REST API that handles HTTP POSTRequest
+    // @PostMapping and @RequesBody
+
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+
+
+
+    }
+
 
 }
